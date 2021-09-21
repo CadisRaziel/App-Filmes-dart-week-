80106006896e9d57a016498ee09fcaef2ab96947
CadisRaziel <vitorcsharp@hotmail.com>
2021-09-21 13:25:06 -0300

    realizando uma simples alteração

--- lib/shared/themes/app_imags.dart
+++ lib/shared/themes/app_imags.dart
@@ -2,5 +2,5 @@ class AppImages {
   static const background = "assets/images/background.png";
   static const logo = "assets/images/logo.png";
   static const imagePesquisa = "assets/images/imagePesquisa.png";
-  static const animacao2 = "assets/lottie/animation2.json";
+  static const animacao2 = "assets/lottie/animation3.json";
 }
